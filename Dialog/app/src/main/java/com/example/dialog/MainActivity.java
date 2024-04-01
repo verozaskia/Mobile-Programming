c871ecb5a78fe6987db6c850f0042607d4f59d91
verozaskia <zqholby@gmail.com>
2024-04-01 15:16:05 +0700

    Menambahkan folder baru

--- Dialog/app/src/main/java/com/example/dialog/MainActivity.java
+++ Dialog/app/src/main/java/com/example/dialog/MainActivity.java
@@ -1,24 +1,76 @@
 package com.example.dialog;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView hasilnya;
+    private EditText txtinput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        hasilnya = findViewById(R.id.result);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.button) {
+                    tampil_input();
+                }
+            }
         });
     }
+
+    private void tampil_input(){
+        LayoutInflater li = LayoutInflater.from(this);
+        View inputnya = li.inflate(R.layout.input_dialog, null);
+
+        AlertDialog.Builder dialognya = new AlertDialog.Builder(this);
+        dialognya.setView(inputnya);
+        final EditText txtinput = (EditText) inputnya.findViewById(R.id.edittext);
+
+        dialognya
+                .setCancelable(false)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        hasilnya.setText(txtinput.getText().toString());
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        dialognya.show();
+    }
+
+    DialogInterface.OnClickListener oknya = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    hasilnya.setText(txtinput.getText().toString());
+                    break;
+                case DialogInterface.BUTTON_NEGATIVE:
+                    // Do nothing, as the action for "Batal" is not specified
+                    break;
+            }
+        }
+    };
 }
