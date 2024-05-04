4631184736c873b77a3f772a671bf60c3830b6cf
verozaskia <zqholby@gmail.com>
2024-05-04 11:31:00 +0700

    Tugas Camera

--- Camera/app/src/main/java/com/example/camera/MainActivity.java
+++ Camera/app/src/main/java/com/example/camera/MainActivity.java
@@ -1,24 +1,51 @@
 package com.example.camera;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1;
+    ImageView iv;
+    private static final int kodekamera = 222;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        b1 = (Button) findViewById(R.id.button);
+        iv = (ImageView) findViewById(R.id.imageView);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(it,kodekamera);
+            }
+        });}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case (kodekamera):prosesKamera(data);break;
+            }}
+    }
+    private void prosesKamera(Intent datanya){
+        Bitmap bm;
+        bm = (Bitmap) datanya.getExtras().get("data");
+        iv.setImageBitmap(bm);
+        Toast.makeText(this,"Data Telah Terload ke ImageView",Toast.LENGTH_SHORT).show();
     }
 }
